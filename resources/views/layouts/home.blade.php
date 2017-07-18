2e91719f0153fb5a36d2e4e2473e0b469c17e69e
qiyeal <702378922@qq.com>
2017-07-18 16:03:42 +0800

    导航笑

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -13,7 +13,7 @@
 </head>
 <body>
 <header>
-    <div id="logo"><a href="{{url('/')}}"></a></div>
+    
     <nav class="topnav" id="topnav">
         @foreach($navs as $k=>$v)<a href="{{$v->nav_url}}"><span>{{$v->nav_name}}</span><span class="en">{{$v->nav_alias}}</span></a>@endforeach
     </nav>
